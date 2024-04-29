f03644e5a7a2a1f385673511d45910b3e947af1c
Avinash Gautam <boss.avinashg@gmail.com>
2024-04-29 17:53:50 +0530

    html file complete

---
+++ scripts/helper.js
@@ -0,0 +1,143 @@
+const formSec = document.getElementById("form-sec");
+const inputs = formSec.querySelectorAll("input");
+const table = document.getElementById("table");
+const recName = "student_records";
+const data = JSON.parse(localStorage.getItem(recName)) ?? [];
+let currentRec = null;
+
+/**
+ * Fetches data from data object & show it in a tabular form
+ */
+function showData() {
+  table.innerHTML = "";
+  if (data.length > 0) {
+    for (const rec of data) {
+      addToTable(rec);
+    }
+  } else {
+    table.innerHTML = `<div class='error'>No Records Found !!!</div>`;
+  }
+}
+
+/**
+ * show or hides the enrollment form
+ * @returns {boolean}
+ */
+const showHideForm = () => formSec.classList.toggle("hide");
+
+/**
+ * check for record in the data using id & return the index
+ * @param {number} id
+ * @return {number}
+ */
+const getRecordIndex = (id) => {
+  return data.findIndex((ele) => ele.id === id);
+};
+
+/**
+ * Performing **ADD, UPDATE & DELETE** operaion on data & updates it to localStorage
+ * - **ADD** - updateData(pos, 0, insertedObj)
+ * - **UPDATE** - updateData(pos, 1, updatedObj)
+ * - **DELETE** - updateData(pos, delCount)
+ *
+ * @param {number} pos index position where to perform operation
+ * @param {number} delCount count of elements to be deleted before operation
+ * @param {object} obj element to insert at given position after deletion
+ */
+const updateData = (pos, delCount, obj = null) => {
+  obj ? data.splice(pos, delCount, obj) : data.splice(pos, delCount);
+  localStorage.setItem(recName, JSON.stringify(data));
+};
+
+/**
+ * Checks for vertical scroll is needed or not on records table
+ */
+const scrollRecord = () => {
+  if (table.parentElement.clientHeight < table.parentElement.scrollHeight) {
+    table.parentElement.style.overflowY = "scroll";
+  } else {
+    table.parentElement.style.overflow = "hidden";
+  }
+};
+
+/**
+ * Adds the record to the table
+ * @param {object} studentObj Object to be inserted
+ */
+function addToTable({ id, name, email, contact }) {
+  table.innerHTML += `
+    <div class="gridRow" id="stuRec-${id}"->
+      <div class="id-col"><b>${id}</b></div>
+      <div class="name-col">${name}</div>
+      <div class="email-col">${email}</div>
+      <div class="contact-col">${contact}</div>
+      <div class="action-col">
+          <i class="icon fas fa-edit" title="Edit" onclick='editRecord(${id})'><span class="sr-only">Edit</span></i>
+          <i class="icon fas fa-minus-square" title="Delete" onclick='deleteRecord(${id})'><span class="sr-only">Delete</span></i>
+      </div>
+    </div>
+    `;
+  scrollRecord();
+}
+
+/**
+ * Edits the record of the data
+ * @param {number} id id of the object to be updated
+ */
+const editRecord = (id) => {
+  const index = getRecordIndex(id);
+  if (index !== -1) {
+    currentRec = data[index];
+    inputs.forEach((input) => {
+      input.value = currentRec[input.name];
+    });
+    submitBtn.innerText = "Update";
+    showHideForm();
+  } else {
+    alert("Invalid Record !!!");
+  }
+};
+
+/**
+ *  Deletes the record after confirmation
+ * @param {number} id id of the object to be deleted
+ */
+const deleteRecord = (id) => {
+  const index = getRecordIndex(id);
+  if (
+    index !== -1 &&
+    confirm(
+      `Do you really want to delete the record?
+        \nRecord:\n${JSON.stringify(data[index]).replaceAll(",", "\n")}`,
+    )
+  ) {
+    table.querySelector(`#stuRec-${id}`).remove();
+    updateData(index, 1);
+    scrollRecord();
+    alert("Record Deleted Successfully !!!");
+    if (!data.length) showData();
+  }
+};
+
+/**
+ * resets the enrollment form
+ */
+const reset = () => {
+  inputs.forEach((input) => {
+    input.value = "";
+    input.setCustomValidity("");
+  });
+  submitBtn.innerText = "Submit";
+  currentRec = null;
+  showHideForm();
+};
+
+/**
+ * checks for validity of input & show the validity message
+ * @param {Event | any} event
+ */
+const showValidityMsg = (event) => {
+  if (!event.target.checkValidity()) {
+    event.target.reportValidity();
+  }
+};
---
+++ scripts/index.js
@@ -0,0 +1,151 @@
+const submitBtn = document.getElementById("submit-btn");
+const closeBtn = document.getElementById("close-btn");
+const addRecBtn = document.getElementById("add-rec-btn");
+const delAllBtn = document.getElementById("del-all-btn");
+
+// input regex for validating the inputs
+const inputRegex = {
+  id: {
+    pattern: /^[1-9]\d*$/,
+    title: "must be greater than 0",
+  },
+  name: {
+    pattern: /^[a-zA-Z]+$/,
+    title: "Enter only firstname (only letters)",
+  },
+  email: {
+    pattern: /^\S+@\S+\.\S{2,3}$/,
+    title: "Enter a valid email",
+  },
+  contact: {
+    pattern: /^[6-9][0-9]{9}$/,
+    title: "Enter a valid contact (only 10 digits)",
+  },
+};
+
+// for showing data on window load
+showData();
+
+/**
+ * Checks for empty inputs
+ *
+ * returns the object constructed by inputs if error does not exist
+ * @returns {null | object}
+ */
+function validateInputs() {
+  let hasError = false;
+  inputs.forEach((input) => {
+    const val = input.value.trim();
+    if (!val) {
+      hasError = true;
+      input.setCustomValidity(`Enter the ${input.name.toUpperCase()}`);
+    }
+  });
+
+  return hasError
+    ? null
+    : {
+        id: inputs[0].valueAsNumber,
+        name: inputs[1].value.trim().toUpperCase(),
+        email: inputs[2].value.trim().toLowerCase(),
+        contact: inputs[3].value.trim(),
+      };
+}
+
+/**
+ * Compare the property value from obj1 with obj2
+ *
+ * return true if both are different else false
+ *
+ * @param {object} obj1
+ * @param {object} obj2
+ * @returns {boolean}
+ */
+const checkForUpdate = (obj1, obj2) => {
+  let isNotEqual = false;
+  if (obj2) {
+    for (const key in obj1) {
+      isNotEqual ||= obj1[key] !== obj2[key];
+    }
+  }
+  return isNotEqual;
+};
+
+/**
+ * Checks for duplicate value & return true if exists or false
+ * @param {boolean} isEditing
+ * @param {number} id1
+ * @param {null | number} id2
+ * @returns {boolean}
+ */
+
+const checkDuplicate = (isEditing, id1, id2) => {
+  return isEditing && id1 === id2 ? false : getRecordIndex(id1) !== -1;
+};
+
+/**
+ * Invokes all user validations on inputs for adding or updating record accordingly
+ * @param {Event} event
+ */
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const studentObj = validateInputs();
+  const isUpdated = checkForUpdate(studentObj, currentRec);
+  const updateAt = isUpdated ? getRecordIndex(currentRec.id) : data.length;
+  const delCount = isUpdated ? 1 : 0;
+
+  // Checking that any updates have been made or not
+  if (currentRec && !isUpdated) {
+    reset();
+  }
+  // Record Duplicacy Check
+  else if (checkDuplicate(isUpdated, studentObj.id, currentRec?.id)) {
+    alert("Duplicate ID Found !!!");
+    inputs[0].setCustomValidity("Enter a unique ID !!!");
+    inputs[0].focus();
+  }
+  // Checking for right object
+  else if (studentObj) {
+    if (table.lastElementChild?.classList.contains("error")) {
+      table.lastElementChild.remove();
+    }
+    addToTable(studentObj);
+    updateData(updateAt, delCount, studentObj);
+    reset();
+    if (isUpdated) {
+      showData();
+    }
+  }
+}
+
+// Event Listeners
+// listener for error checking on every input
+inputs.forEach((input) => {
+  input.oninput = () => {
+    if (!inputRegex[input.name].pattern.test(input.value)) {
+      input.setCustomValidity(inputRegex[input.name].title);
+    } else {
+      input.setCustomValidity("");
+    }
+  };
+  input.addEventListener("mousedown", showValidityMsg);
+  input.addEventListener("focus", showValidityMsg);
+});
+
+addRecBtn.onclick = showHideForm;
+
+// Delete all data from local storage
+delAllBtn.onclick = () => {
+  if (confirm("Do you really want to delete all the records ?")) {
+    updateData(0, data.length);
+    showData();
+    alert("All Records Deleted Succeessfully !!!");
+  }
+};
+
+closeBtn.onclick = reset;
+
+formSec
+  .querySelector("#student-register-form")
+  .addEventListener("submit", handleSubmit);
